408c52f3c14c7302537483b8f2ec987d77a0d396
Julian Churchill <chewielouie03@gmail.com>
2020-02-11 18:53:37 +0000

    Added a cube player and script for arrow controls

---
+++ Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Welcome to cubex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    print("This is a world of cubes");
+
+       var h = Input.GetAxis("Horizontal");
+       var v = Input.GetAxis("Vertical");
+
+       var newX = h * Time.deltaTime;
+       var newY = 0f * Time.deltaTime;
+       var newZ = v * Time.deltaTime;
+
+       transform.Translate(newX, newY, newZ); 
+    }
+}
