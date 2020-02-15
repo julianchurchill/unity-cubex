aa396f151d9cc175e32f649d9379f2ef6e5cd82d
Julian Churchill <chewielouie03@gmail.com>
2020-02-15 12:43:43 +0000

    Added mouse look and player movement

---
+++ Assets/playermovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        var move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
